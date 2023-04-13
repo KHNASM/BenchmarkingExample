cae8e7661e3e93398c84977b4c79beeced14d62b
asm-dev <asm-dev@DESKTOP-AGBLK0J>
2023-04-13 23:14:19 +0100

    Add project files.

---
+++ BenchmarkingExample/Benchmark.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+
+[MemoryDiagnoser]
+public class Benchmark
+{
+    [Benchmark]
+    public void ConcatenateNumbersWithStringBuilder()
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10; i++)
+        {
+            sb.Append(i);
+        }
+    }
+
+    [Benchmark]
+    public void ConcatenateNumbersWithStringJoin()
+    {
+        List<int> list = new List<int>();
+        for (int i = 0; i < 10; i++)
+        {
+            list.Add(i);
+
+        }
+        string concatenatedPrimes = string.Join("", list);
+        
+    }
+}
---
+++ BenchmarkingExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace BenchmarkingExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var results = BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+}
